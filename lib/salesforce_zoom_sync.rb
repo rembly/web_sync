4154437772d0d5934ac923eb4810dd3e8502f8e4
Bryan <b_hermse@hotmail.com>
2018-02-11 20:37:45 -0600

    - Add class and sample config file for sending email summary for nightly job - Add logging and email summary to nightly sync job - Add Pony library for sending SMTP email

--- lib/salesforce_zoom_sync.rb
+++ lib/salesforce_zoom_sync.rb
@@ -1,5 +1,6 @@
 require_relative 'salesforce_sync'
-require_relative './zoom_sync'
+require_relative 'zoom_sync'
+require_relative './web_sync/email_notifier'
 require 'active_support/all'
 require 'pry'
 
@@ -7,28 +8,37 @@ require 'pry'
 # Initially this will be run nightly
 class SalesforceZoomSync
   LOG = Logger.new(File.join(File.dirname(__FILE__), '..', 'log', 'nightly_sync.log'))
-  EMAIL_NOTIFICATION_TO = ENV['EMAIL_NOTIFICATION_TO']
+  EMAIL_NOTIFIER = EmailNotifier.new
 
   attr_accessor :sf
   attr_accessor :zoom_client
   attr_accessor :zoom_users
+  attr_accessor :summary
 
   def initialize
     @sf = SalesforceSync.new
     @zoom_client = ZoomSync.new
     set_zoom_users
+    @summary = ["Nightly Salesforce / Zoom Sync for #{Date.today}"]
+    LOG.info('Starting nightly sync')
     sync_sf_updates_to_zoom
     sync_zoom_updates_to_sf
+    LOG.info('Finished nightly sync')
+    send_summary_email
   end
 
   def sync_sf_updates_to_zoom
+    LOG.info('Syncing Salesforce users to Zoom..')
     # get SF users eligible to be added to zoom based on intro call date and rsvp
     eligible_sf_users = @sf.contacts_eligible_for_zoom
     # add eligibile sf users to zoom if necessary
-    eligible_sf_users.select(&method(:sf_user_not_in_zoom?)).each{|user_to_add| @zoom_client.add_sf_user(user_to_add)}
+    eligible_sf_users.select(&method(:sf_user_not_in_zoom?)).
+                      tap{|users_to_add| log("Adding #{users_to_add.size} SF users to zoom")}.
+                      each{|user_to_add| @zoom_client.add_sf_user(user_to_add)}
   end
 
   def sync_zoom_updates_to_sf
+    LOG.info('Syncing Zoom updates to SF')
     # get all zoom users who have watched the most recent intro call for more than the minimum duration
     sync_intro_call_users
     sleep ZoomSync::MAX_CALLS_PER_SECOND
@@ -37,10 +47,15 @@ class SalesforceZoomSync
 
   private
 
+  def send_summary_email
+    EMAIL_NOTIFIER.send_email(subject: summary[0], body: summary.join("\n"))
+  end
+
   def sync_intro_call_webinar_users
     intro_details = @zoom_client.intro_call_webinar_details
     intro_call_date = intro_details['start_time'].to_date
     intro_call_participants = @zoom_client.intro_call_webinar_participants(meeting_id: intro_details['uuid'])
+    log("Syncing Intro Webinar from #{intro_call_date}")
     add_meeting_participants(intro_call_participants, intro_call_date)
   end
 
@@ -48,19 +63,25 @@ class SalesforceZoomSync
     intro_details = @zoom_client.intro_call_details
     intro_call_date = intro_details['start_time'].to_date
     intro_call_participants = @zoom_client.meeting_participants_report(meeting_id: intro_details['uuid'])
+    log("Syncing Intro Call from #{intro_call_date}")
     add_meeting_participants(intro_call_participants, intro_call_date)
   end
 
   def add_meeting_participants(meeting_participants, intro_call_date)
-    binding.pry
     participants = meeting_participants.dig('participants').select(&method(:valid_intro_call_duration)).
                     select(&method(:valid_zoom_user_for_sf?))
-    # get all SF users matching those participants
     matched_users = @sf.sf_users_for_zoom_users(participants)
+    log("#{participants.size} valid Zoom participants to sync. #{matched_users.size} matched in SF")
     # set the intro call date based for those users. TODO: may need to output multiple users found for same email etc..
     matched_users.each do |user|
       @sf.set_intro_date_for_contact(contact: user, date: intro_call_date)
     end
+  end
+
+  # log messages and build email summary
+  def log(message)
+    LOG.info(message)
+    summary << message
   end
 
   # cache all zoom users, use this rather than re-querying. Maybe only need email address.. for now get everything
---
+++ lib/web_sync/email_notifier.rb
@@ -0,0 +1,23 @@
+require 'net/smtp'
+require 'yaml'
+require 'active_support/all'
+require 'pry'
+
+
+class EmailNotifier
+  LOG = Logger.new(File.join(File.dirname(__FILE__), '..', '..', 'log', 'email.log'))
+  EMAIL_CONFIG = YAML.load_file(File.join(File.dirname(__FILE__), '..', '..', 'config', 'smtp_example.yml'))
+
+  def initialize
+    @to = EMAIL_CONFIG['to']
+    @from = EMAIL_CONFIG['from']
+    @options = EMAIL_CONFIG['via_options'].inject({}){|map, (key, val)| map[key.to_sym] = val; map}
+  end
+
+  def send_email(subject:, body:)
+    LOG.info("To/From: #{@to}/#{@from}, Subject: #{subject}, Body: #{body}, Options: #{@options}")
+    # TODO uncomment when ready to send
+    #Pony.email(body: body, subject: subject, via: :smtp,
+               #via_options: @options
+  end
+end
