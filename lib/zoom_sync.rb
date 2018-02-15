5f4a28b1ae3c03c968bc07ad0340a31127ff8e9c
bhermse <b_hermse@hotmail.com>
2018-02-15 08:45:58 -0600

    - Add intro call meeting details - Add methods for fetching and setting meeting registrants instead of users - Add ability to add meeting registrant - Update post method to also accept query parameters. This was needed for meeting instance id

--- lib/zoom_sync.rb
+++ lib/zoom_sync.rb
@@ -20,10 +20,10 @@ class ZoomSync
   BASIC_USER_TYPE = 1
   # TODO: update with actual intro call ID when available
   INTRO_CALL_MEETING_ID = '2017201719'
-  INTRO_WEBINAR_ID = '891518756' # sample webinar
+  CLIMATE_ADVOCACY_WEBINAR_ID = '891518756' # sample webinar
   INTRO_MEETING_ID = '526383982' # sample recurring meeting
-  PRODUCTION_INTRO_MEETING_ID = '847350531' # sample recurring meeting
-  MINIMUM_DURATION_FOR_INTRO_CALL = 10 # minutes
+  INTRO_WEBINAR_ID = '847350531' # occurance 1519261200000
+  MINIMUM_DURATION_FOR_INTRO_CALL = 600 # seconds / 10 minutes
 
   # queue for rate_limited api calls
   attr_reader :call_queue
@@ -57,11 +57,11 @@ class ZoomSync
   end
 
   # create an account and send email
-  def post(endpoint:, data:)
-    base_uri = URI.join(ZOOM_API_URL, endpoint).to_s
+  def post(endpoint:, data:, params: {})
+    base_uri = [URI.join(ZOOM_API_URL, endpoint).to_s, params.to_query].compact.join('?')
     begin
       # TODO: uncomment to enable zoom account creation. Account creation will send email to client
-      #RestClient.post(base_uri, data.to_json, {content_type: :json, accept: :json, Authorization: "Bearer #{@zoom_web_token}"})
+      RestClient.post(base_uri, data.to_json, {content_type: :json, accept: :json, Authorization: "Bearer #{@zoom_web_token}"})
       LOG.info("Zoom client to be created: #{data}")
     rescue RestClient::ExceptionWithResponse => e
       return handle_response(e.response)
@@ -69,9 +69,9 @@ class ZoomSync
   end
 
   # schedule update for later, taking API limit into account. Passes results of update to optional callback
-  def queue_post(endpoint:, data:, &callback)
+  def queue_post(endpoint:, data:, params: {}, &callback)
     @call_queue << lambda {
-      results = post(endpoint: endpoint, data: data)
+      results = post(endpoint: endpoint, data: data, params: params)
       callback.call(results) if callback.present?
     }
   end
@@ -92,7 +92,7 @@ class ZoomSync
 
     if success_response?(response)
       results = JSON.parse(response)
-      return gather_pages?(results) ? merge_participants(results, get_next_page(response, results)) : results
+      return gather_pages?(results) ? merge_users(results, get_next_page(response, results)) : results
     else
       # TODO: potentially handle 429 rate error by delaying/resending
       LOG.error("FAILED request #{response.request}: MESSAGE: #{JSON.parse(response)}")
@@ -108,10 +108,6 @@ class ZoomSync
 
   def meeting_report_for(from: Date.today - 2.months, to: Date.today - 1.month)
     call(endpoint: 'metrics/meetings', params: { from: from.to_s, to: to.to_s })
-  end
-
-  def meeting_details(meeting_id:)
-    call(endpoint: "meetings/#{meeting_id}")
   end
 
   def daily_report(date: Date.today - 1.month)
@@ -130,24 +126,26 @@ class ZoomSync
     call(endpoint: 'users/')
   end
 
-  def meeting_participants_report(meeting_id:)
-    call(endpoint: "report/meetings/#{meeting_id}/participants")
+  def webinar_participants_report(id:)
+    call(endpoint: "report/webinars/#{id}/participants")
   end
 
-  def intro_call_meeting_participants
-    meeting_participants_report(meeting_id: INTRO_CALL_MEETING_ID)
+  # get upcoming occurrences
+  def climate_advocacy_details
+    call(endpoint: "webinars/#{CLIMATE_ADVOCACY_WEBINAR_ID}")
   end
 
-  def intro_call_details
-    meeting_details(meeting_id: INTRO_CALL_MEETING_ID)
+  # this defaults to 'approved' registrants only. But all invited are auto-approved at this point
+  def climate_advocacy_registrants
+    call(endpoint: "webinars/#{CLIMATE_ADVOCACY_WEBINAR_ID}/registrants")
   end
 
   def intro_call_webinar_details
     call(endpoint: "webinars/#{INTRO_WEBINAR_ID}")
   end
 
-  def intro_call_webinar_participants(meeting_id:)
-    call(endpoint: "report/webinars/#{meeting_id}/participants")
+  def climate_advocacy_participants
+    webinar_participants_report(id: CLIMATE_ADVOCACY_WEBINAR_ID)
   end
 
   # this will send an invite to the passed in SF user's primary email to join zoom
@@ -163,6 +161,18 @@ class ZoomSync
     }
 
     queue_post(endpoint: 'users/', data: data) { |results| LOG.debug("Post results: #{results}") }
+  end
+
+  # note that this triggers welcome meeting and adds them in 'approved' status
+  def add_intro_meeting_registrant(sf_user, meeting_occurrence)
+    LOG.info("SF user not found in zoom. Adding to Zoom")
+    data = {
+        email: SalesforceSync.primary_email(sf_user),
+        first_name: sf_user.FirstName,
+        last_name: sf_user.LastName,
+    }
+
+    queue_post(endpoint: "webinars/#{INTRO_WEBINAR_ID}/registrants", data: data, params: {occurrence_ids: meeting_occurrence}) { |results| LOG.debug("Add registrant results: #{results}") }
   end
 
   private
@@ -184,7 +194,7 @@ class ZoomSync
 
   # gather pages if there is a next_page token and the result set contains participants
   def gather_pages?(results)
-    results.dig('next_page_token').present? && results.dig('participants').present?
+    results.dig('next_page_token').present? && (results.dig('participants').present? || results.dig('registrants').present?)
   end
 
   # get next page by re-sending same request but with next page token. This will block for max api call rate duration
@@ -197,9 +207,9 @@ class ZoomSync
     call(endpoint: endpoint, params: params.merge({next_page_token: results.dig('next_page_token')}))
   end
 
-  # merge the participant list of two result sets
+  # merge the participant or registrant list of two result sets.
   # throw if one result set is an error?
-  def merge_participants(r1, r2)
-    r1.deep_merge(r2){|key, r1, r2| key == 'participants' ? r1 + r2 : r2}
+  def merge_users(r1, r2)
+    r1.deep_merge(r2){|key, r1, r2| %w(participants registrants).include?(key) ? r1 + r2 : r2}
   end
 end
