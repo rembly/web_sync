0940a1fd5b2e41b3077de364a06e68186b37bfec
Bryan <b_hermse@hotmail.com>
2018-03-13 08:34:03 -0500

    Update selection of production/dev for salesforce oauth token

--- lib/web_sync/oauth_token.rb
+++ lib/web_sync/oauth_token.rb
@@ -8,12 +8,13 @@ class OauthToken
   SALESFORCE_SECRET = ENV['SALESFORCE_SECRET']
   SALESFORCE_CLIENT_ID = ENV['SALESFORCE_CLIENT_ID']
   SALESFORCE_USER = ENV['SALESFORCE_USER']
+  IS_PRODUCTION = ENV['ENVIRONMENT'].to_s == 'production'
 
   SANDBOX_TOKEN_URL = 'https://test.salesforce.com/services/oauth2/token'
   PRODUCTION_TOKEN_URL = 'https://login.salesforce.com/services/oauth2/token'
 
-  def self.salesforce_token(production = false)
-    token_url = production ? PRODUCTION_TOKEN_URL : SANDBOX_TOKEN_URL
+  def self.salesforce_token
+    token_url = IS_PRODUCTION ? PRODUCTION_TOKEN_URL : SANDBOX_TOKEN_URL
     p "Fetching salesforce token from #{token_url}"
     begin
       response = RestClient.post(token_url, { grant_type: 'password', client_id: SALESFORCE_CLIENT_ID, client_secret: SALESFORCE_SECRET,
