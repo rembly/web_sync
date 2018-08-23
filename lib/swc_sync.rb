fb70ca91afde751e1d4519e4208fa56b65684f58
Bryan Hermsen <b_hermse@hotmail.com>
2018-08-23 11:27:55 -0500

    Allow for migrating buddy drive files to SWC: - Migration method: - Export buddydrive metadata from Wordpress with columns matching DRIVE_COLUMNS - Point ENV['BUDDY_FILE_PATH'] to exported BuddyDrive folder with physical files - Run upload_file() which will match users in SWC based on their email address - Files for that user will be uploaded to the "my files" section of the user's SWC profile as private files

--- lib/swc_sync.rb
+++ lib/swc_sync.rb
@@ -5,24 +5,28 @@ require 'json'
 require 'active_support/all'
 require_relative 'web_sync/json_web_token'
 require_relative './zoom_sync'
+require 'csv'
 
 # Rate Limiting Headers
 # X-Rate-Limit-Limit – Number of requests allowed in the time frame 100 requests per 60 seconds
 # X-Rate-Limit-Remaining – Number of requests let in the current time frame
 # X-Rate-Limit-Reset – Seconds left in the current me frame
+# TODO: Abstract Rest Methods and Throttling to base class with ZoomSync
 class SwcSync
   LOG = Logger.new(File.join(File.dirname(__FILE__), '..', 'log', 'swc.log'))
   HALT_CALL_QUEUE_SIGNAL = :stop
   MAX_CALLS_PER_SECOND = 1.5 # 1 second limit plus buffer
   API_URL = 'http://cclobby.smallworldlabs.com/services/4.0/'
-
-  FILE_CATEGORIES = {1: 'pdf, pptx, docx', 2: }
+  FILES_PATH = ENV['BUDDY_FILE_PATH']
+  BUDDY_FILES_CSV = File.join(File.dirname(__FILE__), '..', 'data', 'buddy_drive_files.csv')
+  DRIVE_COLUMNS = %i[id email path title description mime_type].freeze
 
   # queue for rate_limited api calls
   attr_reader :call_queue
   attr_reader :last_response
   attr_accessor :queue_consumer
   attr_accessor :swc_token
+  attr_accessor :users
 
   def initialize
     @swc_token = JsonWebToken.swc_token
@@ -30,19 +34,38 @@ class SwcSync
     @queue_consumer = start_request_queue_consumer
   end
 
-  def add_file(file)
-   # public: false
-   # type: mime-type
-   # file, title, public
-
+  def get_users
+    @users.present? ? @users : @users = call(endpoint: 'users')
   end
 
-  # look for next_page_token to know whether to page. Max is 300.. may not hit this?
+  def upload_files
+    CSV.foreach(BUDDY_FILES_CSV) { |row| upload_file(row[1], row[2], row[3], row[4]) }
+  end
+
+  def upload_file(email, url, title, description)
+    id = find_user_id_from_email(email)
+    if id.present?
+      filename = File.basename(URI.parse(url)&.path)
+      file = File.new(File.join(FILES_PATH, filename), 'rb')
+      # LOG.info("User/File found for #{email} / #{filename}")
+      begin
+        RestClient.post(URI.join(API_URL, 'files').to_s, { file: file, title: title, description: description,
+                                                           public: false, userId: id }, Authorization: "Bearer #{swc_token}")
+      #   LOG.info("User/File uploaded #{email} / #{filename}")
+      rescue RestClient::ExceptionWithResponse => e
+        return handle_response(e.response)
+      end
+   end
+  end
+
+  def find_user_id_from_email(email)
+    get_users.find { |u| u['emailAddress'] == email }.try(:dig, 'userId')
+  end
+
   def call(endpoint:, params: {})
     base_uri = URI.join(API_URL, endpoint).to_s
-   #  params = params.merge(access_token: @swc_token)
     begin
-      response = RestClient.get(base_uri, {Authorization: "Bearer #{swc_token}", params: params})
+      response = RestClient.get(base_uri, Authorization: "Bearer #{swc_token}", params: params)
       return handle_response(response)
     rescue RestClient::ExceptionWithResponse => e
       return handle_response(e.response)
@@ -57,6 +80,24 @@ class SwcSync
     }
   end
 
+  # create an account and send email
+  def post(endpoint:, data:, params: {})
+    base_uri = [URI.join(API_URL, endpoint).to_s, params.to_query].compact.join('?')
+    begin
+      RestClient.post(base_uri, data.to_json, content_type: :json, accept: :json, Authorization: "Bearer #{swc_token}")
+    rescue RestClient::ExceptionWithResponse => e
+      return handle_response(e.response)
+    end
+   end
+
+  # schedule update for later, taking API limit into account. Passes results of update to optional callback
+  def queue_post(endpoint:, data:, params: {}, &callback)
+    @call_queue << lambda {
+      results = post(endpoint: endpoint, data: data, params: params)
+      yield(results) if callback.present?
+    }
+  end
+
   # return json representation of results or error object if call failed. This will handle client pagination
   def handle_response(response)
     return if response.blank?
@@ -64,9 +105,8 @@ class SwcSync
 
     if success_response?(response)
       return JSON.parse(response)
-      # return gather_pages?(results) ? merge_users(results, get_next_page(response, results)) : results
     else
-      # TODO: potentially handle 429 rate error by delaying/resending
+      # TODO: potentially handle rate limit
       LOG.error("FAILED request #{response.request}: MESSAGE: #{JSON.parse(response)}")
       return JSON.parse(response)
     end
