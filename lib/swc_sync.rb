0338dd3a9199afc44f64a12393c3c3170e9af171
Bryan Hermsen <b_hermse@hotmail.com>
2018-08-29 12:13:10 -0500

    Additional methods for building import files for SWC migration

--- lib/swc_sync.rb
+++ lib/swc_sync.rb
@@ -5,6 +5,7 @@ require 'json'
 require 'active_support/all'
 require_relative 'web_sync/json_web_token'
 require_relative './zoom_sync'
+require_relative 'salesforce_sync'
 require 'csv'
 
 # Rate Limiting Headers
@@ -18,8 +19,19 @@ class SwcSync
   MAX_CALLS_PER_SECOND = 1.5 # 1 second limit plus buffer
   API_URL = 'http://cclobby.smallworldlabs.com/services/4.0/'
   FILES_PATH = ENV['BUDDY_FILE_PATH']
+  CHAPTER_FILE_LOCATION = File.join(File.dirname(__FILE__), '..', 'data', 'chapter_import.json')
   BUDDY_FILES_CSV = File.join(File.dirname(__FILE__), '..', 'data', 'buddy_drive_files.csv')
   DRIVE_COLUMNS = %i[id email path title description mime_type].freeze
+  DEFAULT_CATEGORY = '1'
+  SWC_CONTACT_FIELDS = %w[Email FirstName LastName Region__c Group_del__c SWC_Create_User__c SWC_Chapter_Status__c SWC_User_ID__c SWC_Regional_Coordinator__c SWC_Staff__c SWC_State_Coordinator__c SWC_Liaison__c SWC_Group_Leader__c Country_Full_Name__c Group_del__r.SWC_Group_ID__c Group_Leader_del__c].freeze
+  SWC_CONTACT_IMPORT_COLUMNS = %w[*_email_address *_username *_first_name *_last_name o_date_joined o_language o_signature o_segments o_avatar_file_location o_groups o_privacy_send_mail o_privacy_profile_posts o_privacy_profile_posts_details o_privacy_my_files o_privacy_my_photos o_privacy_my_videos o_notification_emails o_notification_profile o_notification_photos o_notification_multimedia o_notification_files	o_notification_blogs o_notification_events o_notification_groups o_notification_reviews o_notification_forums o_notification_likes o_notification_mentions o_privacy_About_Me o_privacy_CCL_Info	o_privacy_Address	o_privacy_Phone o_privacy_Email o_privacy_Social_Media o_privacy_My_Interests].freeze
+  SWC_GROUP_IMPORT_COLUMNS = %w[o_group_id *_name *_description *_category_id *_owner_user_id o_access_level o_address o_invite_message o_welcome_message o_news o_content_forums o_content_invite o_content_events o_content_photos o_content_videos o_content_files o_content_members o_content_blogs o_photo_location].freeze
+
+  GROUP_DESCRIPTION_TEXT = "Welcome to Citizens\xE2\x80\x99 Climate Lobby %s! We work within our community and our local members of Congress towards enacting a big, national solution to climate change. We work by building relationships and finding common ground. We welcome anyone in the area who likes this approach to join us."
+  GROUP_INVITE_TEXT = "Sign up to get started with CCL. We\xE2\x80\x99ll connect you with your local group, and provide you with information about the training and resources you need."
+  GROUP_NEWS_TEXT = "Most chapters meet monthly on or near the second Saturday of the month. Contact %s to get details on this chapter's meeting schedule."
+  GROUP_CHAPTER_CATEGORY = 4
+  GROUP_DEFAULT_OWNER = 1
 
   # queue for rate_limited api calls
   attr_reader :call_queue
@@ -27,16 +39,66 @@ class SwcSync
   attr_accessor :queue_consumer
   attr_accessor :swc_token
   attr_accessor :users
+  attr_accessor :sf
 
   def initialize
     @swc_token = JsonWebToken.swc_token
     @call_queue = Queue.new
     @queue_consumer = start_request_queue_consumer
+    @sf = SalesforceSync.new
   end
 
   def get_users
     @users.present? ? @users : @users = call(endpoint: 'users')
   end
+
+  def get_sf_contacts(_where)
+    contacts = @sf.client.query(<<-QUERY)
+      SELECT #{SWC_CONTACT_FIELDS.join(', ')}
+      FROM Contact
+      WHERE LastName = 'Hermsen' AND Is_CCL_Supporter__c = True
+    QUERY
+  end
+
+  def build_ccl_member_import(query); end
+
+  def build_ccl_chapter_import
+    # selects active and in-progress chapters. TODO
+    chapters = sf.ccl_chapters
+    import_string = chapters.collect(&method(:get_chapter_row))
+    p import_string
+    File.open(CHAPTER_FILE_LOCATION, 'w') { |f| f.puts(import_string.to_json) }
+  end
+
+  # this would be csv import
+  # %w[o_group_id *_name *_description *_category_id *_owner_user_id o_access_level o_address o_invite_message o_welcome_message o_news o_content_forums o_content_invite o_content_events o_content_photos o_content_videos o_content_files o_content_members o_content_blogs o_photo_location].freeze
+  # 'o_address': { 'line1': '', 'line2': '', 'city': ch.City__c, 'state': ch.State__c, 'zip': '',
+  #                'country': !ch.Country__c.nil? ? ch.Country__c : 'USA' },
+  def get_chapter_row(ch)
+    # TODO: country
+    { '*_name': ch.Name, '*_description': GROUP_DESCRIPTION_TEXT % ch.Name, '*_category_id': GROUP_CHAPTER_CATEGORY,
+      '*_owner_user_id': GROUP_DEFAULT_OWNER, 'o_access_level': ch.Creation_Stage__c == 'In-Active' ? '3' : '1',
+      'o_address': ",,#{ch.City__c}, #{ch.State__c},, #{!ch.Country__c.nil? ? ch.Country__c : 'USA'}",
+      'o_news': GROUP_NEWS_TEXT % (ch.Group_Email__c || 'chapter@ccl.org'),
+      'o_content_forums': '1', 'o_content_invite': '2', 'o_content_events': '0', 'o_content_photos': '2',
+      'o_content_videos': '1', 'o_content_files': '1', 'o_content_members': '2' }
+  end
+
+  def get_chapter_object(ch)
+    {
+      'name': ch.Name, 'description': GROUP_DESCRIPTION_TEXT % ch.Name, 'categoryId': GROUP_CHAPTER_CATEGORY,
+      'ownerId': GROUP_DEFAULT_OWNER, 'access': ch.Creation_Stage__c == 'In-Active' ? '3' : '1',
+      'address': { 'line1': '', 'line2': '', 'city': ch.City__c, 'state': ch.State__c, 'zip': '',
+                   'country': !ch.Country__c.nil? ? ch.Country__c : 'USA' },
+      'news': GROUP_NEWS_TEXT % (ch.Group_Email__c || 'chapter@ccl.org'),
+      'o_content_forums': '1', 'o_content_invite': '2', 'o_content_events': '0', 'o_content_photos': '2',
+      'o_content_videos': '1', 'o_content_files': '1', 'o_content_members': '2'
+    }
+   end
+
+  # https://docs.google.com/document/d/1I56rG3YB_618RCeip_9sY5u5JiFagdY1JBhEvcbWhLk/edit
+  # description = about_us copy from above
+  # news = default chapter news - insert chapter email address
 
   def upload_files
     CSV.foreach(BUDDY_FILES_CSV) { |row| upload_file(row[1], row[2], row[3], row[4]) }
@@ -50,7 +112,9 @@ class SwcSync
       # LOG.info("User/File found for #{email} / #{filename}")
       begin
         RestClient.post(URI.join(API_URL, 'files').to_s, { file: file, title: title, description: description,
-                                                           public: false, userId: id }, Authorization: "Bearer #{swc_token}")
+                                                           public: false, userId: id,
+                                                           categoryId: DEFAULT_CATEGORY },
+                        Authorization: "Bearer #{swc_token}")
       #   LOG.info("User/File uploaded #{email} / #{filename}")
       rescue RestClient::ExceptionWithResponse => e
         return handle_response(e.response)
--- lib/web_sync/oauth_token.rb
+++ lib/web_sync/oauth_token.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rest-client'
 require 'json'
 require 'active_support/all'
@@ -15,16 +17,17 @@ class OauthToken
   SANDBOX_TOKEN_URL = 'https://test.salesforce.com/services/oauth2/token'
   PRODUCTION_TOKEN_URL = 'https://login.salesforce.com/services/oauth2/token'
 
-  SERVICE_ACCOUNT_KEY_FILE = 'google_sheet_sync.json'.freeze
+  SERVICE_ACCOUNT_KEY_FILE = 'google_sheet_sync.json'
   GOOGLE_SHEET_READONLY_SCOPE = 'https://www.googleapis.com/auth/spreadsheets.readonly'
   GOOGLE_SHEEET_READWRITE_SCOPE = 'https://www.googleapis.com/auth/spreadsheets'
+  FUSION_TABLE_READWRITE_SCOPE = 'https://www.googleapis.com/auth/fusiontables'
 
   def self.salesforce_token
     token_url = IS_PRODUCTION ? PRODUCTION_TOKEN_URL : SANDBOX_TOKEN_URL
     p "Fetching salesforce token from #{token_url}"
     begin
-      response = RestClient.post(token_url, { grant_type: 'password', client_id: SALESFORCE_CLIENT_ID, client_secret: SALESFORCE_SECRET,
-                                                      username: SALESFORCE_USER, password: SALESFORCE_PWD})
+      response = RestClient.post(token_url, grant_type: 'password', client_id: SALESFORCE_CLIENT_ID, client_secret: SALESFORCE_SECRET,
+                                            username: SALESFORCE_USER, password: SALESFORCE_PWD)
       return JSON.parse(response.body)
     rescue RestClient::ExceptionWithResponse => e
       e.response
@@ -32,10 +35,10 @@ class OauthToken
   end
 
   def self.google_service_token
-    authorizer = Google::Auth::ServiceAccountCredentials.make_creds( 
-      json_key_io: File.open(SERVICE_ACCOUNT_KEY_FILE), scope: [GOOGLE_SHEET_READONLY_SCOPE])
+    authorizer = Google::Auth::ServiceAccountCredentials.make_creds(
+      json_key_io: File.open(SERVICE_ACCOUNT_KEY_FILE), scope: [GOOGLE_SHEET_READONLY_SCOPE, FUSION_TABLE_READWRITE_SCOPE]
+    )
     authorizer.fetch_access_token!
     authorizer
   end
-
 end
