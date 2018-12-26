b1bfc79abe5b44e1c2b6fb9ed006f4d3727e2bd9
Bryan <b_hermse@hotmail.com>
2018-12-26 16:22:02 -0600

    Update endorser sync to filter for vetted endorsers

--- lib/google_sync.rb
+++ lib/google_sync.rb
@@ -12,10 +12,12 @@ class GoogleSync
    APPLICATION_NAME = ENV['GOOGLE_APP_NAME']
    LOG = Logger.new(File.join(File.dirname(__FILE__), '..', 'log', 'sync_endorsers.log'))
    ENDORSER_SHEET_ID = ENV['GOOGLE_ENDORSER_SHEET_ID']
-   ENDORSER_DATA_RANGE = 'Ready for Web!A2:AE'.freeze
-   COLUMN_HEADING_RANGE = 'Ready for Web!A1:AE1'.freeze
-   COLUMN_HEADINGS = ['Submitted Date','Completion Time','Completion Status',"I'm Endorsing As...",'First Name ','Last Name','Title','Email','Phone','Organization Name','Organization Name','Website URL','Address Line 1','Address Line 1','Address Line 2','City','State','Postal Code','Organization Type','# of U.S. employees','Population','Comments (not required)','Please confirm','Response Url','Referrer','Ip Address','Unprotected File List','Reviewer','Status','Notes','Featured Endorser']
+   ENDORSER_DATA_RANGE = 'Ready for Web!A2:AF'.freeze
+   COLUMN_HEADING_RANGE = 'Ready for Web!A1:AF1'.freeze
+   COLUMN_HEADINGS = ['Submitted Date','Completion Time','Completion Status',"I'm Endorsing As...",'First Name ','Last Name','Title','Email','Phone','Organization Name','Organization Name','Website URL','Address Line 1','Address Line 1','Address Line 2','City','State','Postal Code','Organization Type','# of U.S. employees','Population','Comments (not required)','Please confirm','Response Url','Referrer','Ip Address','Unprotected File List','Reviewer','Status','Notes','Featured Endorser', 'Final Staff Check']
    WP_ENDORSER_TABLE = 'bill_endorsers'
+
+   FINAL_STAFF_CHECK = 31
 
    attr_accessor :google_client
    attr_accessor :token
@@ -31,15 +33,15 @@ class GoogleSync
    def sync_endorsers
       return unless valid_data_columns?
 
-      current_endorsers = get_current_endorser_data.map(&method(:endorser_row))
+      current_endorsers = get_current_endorser_data.select(&method(:include_row?)).map(&method(:endorser_row))
       clear_wp_endorsers
       
       begin
          wp_client.query(<<-INSERT)
             INSERT INTO #{WP_ENDORSER_TABLE}(first_name, last_name, title, organization_name,
-               website_url, city, state, postal_code, organization_type)
+               website_url, city, state, postal_code, organization_type, featured_endorser, individual_organization)
             VALUES
-               #{current_endorsers.map{|endorser_row| "('#{endorser_row.join("','")}')"}.join(',')}
+               #{current_endorsers.map{|endorser_row| '("' + endorser_row.join('","') + '")'}.join(',')}
          INSERT
          
          LOG.info("Synchronized #{current_endorsers.size} endorsers to Wordpress")
@@ -49,17 +51,25 @@ class GoogleSync
       end
    end
 
+
+   def include_row?(row)
+      row[31].to_s.strip.casecmp('x').zero?
+   end
+
    def endorser_row(row)
+      is_org = row[3] == 'An organization'
       endorsing_as = row[3]
       org_type = row[18]
       first_name, last_name, title = row[4], row[5], row[6]
-      org_name = row[9].present? ? row[9] : row[10]
+      org = row[9].present? ? row[9] : row[10]
+      org_name = is_org ? org : '';
       website_url = row[11]
       city, state, zip = row[15], row[16], row[17]
       comments = row[21]
       # submitted_at = row[0]
+      featured_endorser = row[30].to_s.present? ? 1 : 0;
 
-      [first_name, last_name, title, org_name, website_url, city, state, zip, org_type]
+      [first_name, last_name, title, org_name, website_url, city, state, zip, org_type, featured_endorser, org]
    end
 
    def clear_wp_endorsers
