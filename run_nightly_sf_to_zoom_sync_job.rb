ad2c0be3514bcfbbdf5df2811c369ef3f6217145
Bryan Hermsen <b_hermse@hotmail.com>
2019-07-23 14:50:42 -0500

    Add email alert for backtrace failure.

--- run_nightly_sf_to_zoom_sync_job.rb
+++ run_nightly_sf_to_zoom_sync_job.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 require_relative './lib/salesforce_zoom_sync'
+require_relative './lib/web_sync/email_notifier'
 
 Process.setproctitle('nightly_sync')
 
@@ -13,8 +14,13 @@ begin
   sync.run_sf_to_zoom_sync
   sync.run_ocat_zoom_to_sf_sync
 rescue Exception => e
-  p "Daily SF to Zoom Sync Job Failed: #{e.message}"
-  p e.backtrace.inspect
+  message = "Daily SF to Zoom Sync Job Failed: #{e.message}"
+  backtrace = e.backtrace.inspect
+  p message
+  p backtrace
   LOG.error("Daily SF to Zoom Sync Job Failed: #{e.message}")
-  LOG.error(e.backtrace.inspect)
+  LOG.error(backtrace)
+  to = 'bryan.hermsen@citizensclimate.org'
+  EmailNotifier.new.send_email(subject: 'Nightly SF to Zoom Sync Failure', 
+    body: "Failed to complete nightly sync: #{message}\nBacktrace: #{backtrace}", to: to)
 end
